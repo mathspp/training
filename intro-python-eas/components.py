1525269dae08058c52f5ed5713c3793f3ad6dd7b
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-02-23 19:28:22 +0000

    Finish rendering function

--- intro-python-eas/components.py
+++ intro-python-eas/components.py
@@ -1,17 +1,19 @@
 import pygame
-from pygame.locals import *
+import pygame.locals
 
-from random import random
+pygame.display.init()
+
+from random import random, choice
 
 class Directions(object):
     """Hold the directions in which the robot can walk."""
 
     LEFT = [-1, 0]
     RIGHT = [1, 0]
-    UP = [0, 1]
-    DOWN = [0, -1]
+    UP = [0, -1]
+    DOWN = [0, 1]
 
-def generate_map(width, height):
+def generate_room(width, height):
     """Generate a 2D array of dimensions height x width with random values."""
     return [[random() for _ in range(width)] for _ in range(height)]
 
@@ -41,5 +43,128 @@ def score_robot(robot, room):
 
     return score, room_copy
 
-def render_simulation(simulation):
-    pass
+# pygame-related global variables
+FPS = 60
+SQUARESIZE = 40
+VELOCITY = 2
+WAITING = 3
+ROBOT_COLOUR = (230, 230, 0)
+WHITE = (255, 255, 255)
+
+def draw_room(screen, room):
+    """Takes a room and draws it."""
+    rect = pygame.Rect(0, 0, SQUARESIZE, SQUARESIZE)
+
+    for y, row in enumerate(room):
+        for x, value in enumerate(row):
+            rect.left = x * SQUARESIZE
+            rect.top = y * SQUARESIZE
+            # the level of dirtiness shows in different shades of gray
+            gray = [int((1 - value) * 255)] * 3
+            pygame.draw.rect(screen, gray, rect)
+
+def draw_nice_rect(screen, width, height, colour, rect):
+    """Draws a rectangle nicely in the screen with the given dimensions.
+
+    If needed, wrap the drawing around the screen.
+    """
+
+    top_bot_parts = []
+
+    # Check if we need to split the rectange horizontally
+    if rect.bottom > height:
+        top_bot_parts.append(
+            pygame.Rect(rect.left, rect.top, rect.width, height - rect.top)
+        )
+        top_bot_parts.append(
+            pygame.Rect(rect.left, 0, rect.width, rect.height + rect.top - height)
+        )
+    else:
+        top_bot_parts.append(
+            pygame.Rect(rect.left, rect.top, rect.width, rect.height)
+        )
+
+    parts = []
+    for rect in top_bot_parts:
+        if rect.right > width:
+            parts.append(
+                pygame.Rect(rect.left, rect.top, width - rect.left, rect.height)
+            )
+            parts.append(
+                pygame.Rect(rect.left, rect.top, rect.width + rect.left - width, rect.height)
+            )
+        else:
+            parts.append(
+                rect
+            )
+
+    for rect in parts:
+        pygame.draw.rect(screen, colour, rect)
+
+def render_simulation(robot, room):
+    """Use pygame to render the robot cleaning a given room."""
+
+    width = len(room[0])
+    height = len(room)
+
+    WIDTH = SQUARESIZE * width
+    HEIGHT = SQUARESIZE * height
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    draw_room(screen, room)
+
+    clock = pygame.time.Clock()
+
+    robot_left = (width // 2) * SQUARESIZE
+    robot_top = (height // 2) * SQUARESIZE
+    robot_rect = pygame.Rect(robot_left, robot_top, SQUARESIZE, SQUARESIZE)
+
+    arrived = True # flags if the robot arrived at a square
+    robot_idx = 0
+    waiting = 0 # number of frames paused between directions
+    direction = None # the direction we are moving in
+
+    going = True
+    paused = False
+    while going:
+        clock.tick(FPS)
+
+        for ev in pygame.event.get():
+            if ev.type == pygame.locals.QUIT:
+                going = False
+            elif ev.type == pygame.locals.KEYDOWN:
+                if ev.key == pygame.locals.K_p or ev.key == pygame.locals.K_SPACE:
+                    paused = not paused
+
+        # Only try moving the robot while there are movements to consider
+        if robot_idx < len(robot) and not paused:
+            if waiting:
+                waiting -= 1
+
+            elif arrived:
+                direction = robot[robot_idx]
+                arrived = False
+
+                if direction[0] == -1:
+                    dir_name = "left"
+                elif direction[0] == 1:
+                    dir_name = "right"
+                elif direction[1] == -1:
+                    dir_name = "up"
+                elif direction[1] == 1:
+                    dir_name = "down"
+                pygame.display.set_caption("Movement {} going {}".format(robot_idx, dir_name))
+
+            else:
+                draw_nice_rect(screen, WIDTH, HEIGHT, WHITE, robot_rect)
+
+                robot_rect.left = (robot_rect.left + VELOCITY*direction[0]) % WIDTH
+                robot_rect.top = (robot_rect.top + VELOCITY*direction[1]) % HEIGHT
+
+                if (robot_rect.left % SQUARESIZE == 0) and (robot_rect.top % SQUARESIZE == 0):
+                    arrived = True
+                    waiting = WAITING
+                    robot_idx += 1
+
+                draw_nice_rect(screen, WIDTH, HEIGHT, ROBOT_COLOUR, robot_rect)
+
+        pygame.display.flip()
