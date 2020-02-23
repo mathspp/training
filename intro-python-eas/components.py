451fb7ed81795eeca337a9ce59e9015e6344b427
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-02-23 18:17:38 +0000

    Add docs for a function

--- intro-python-eas/components.py
+++ intro-python-eas/components.py
@@ -16,10 +16,11 @@ def generate_map(width, height):
     return [[random() for _ in range(width)] for _ in range(height)]
 
 def score_robot(robot, room):
-    """Given a room and a cleaning robot, score it.
+    """Given a room and a cleaning robot, score the cleaning of the robot.
     
     The robot is a list of Directions.
-    The room is a 2D array with dimensions height x width.
+    The room is a 2D array with dimensions height x width. The room is seen
+        as a torus, meaning walking off the edge wraps around.
     """
     
     score = 0
@@ -31,10 +32,12 @@ def score_robot(robot, room):
     room_copy = [[val for val in row] for row in room]
 
     for dx, dy in robot:
-        posx = (posx + dx) % width
-        posy = (posy + dy) % height
         score += room_copy[posy][posx]
         room_copy[posy][posx] = 0
+        posx = (posx + dx) % width
+        posy = (posy + dy) % height
+    score += room_copy[posy][posx]
+    room_copy[posy][posx] = 0
 
     return score, room_copy
 
